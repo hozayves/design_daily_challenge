92b968bb5a0c8a5b4d0560fa3255e13a8f79a533
hozayves <muhozayves2014@gmail.com>
2023-10-01 22:25:43 +0200

    Initial commit job cards challenges : fire:

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import JobCard from './components/JobCard';
+import design from './dist/assets/Frame.png';
+import engineer from './dist/assets/Frame-1.png';
+import operation from './dist/assets/Frame-2.png';
+import people from './dist/assets/Frame-3.png';
+import sales from './dist/assets/Frame-4.png';
+import university from './dist/assets/Frame-5.png';
+
+const position = [
+  {name: "Design", nums: 4, icon: design, bgGradient: "bg-bgDesign", bgPosition: null},
+  {name: "Engineering", nums: 8, icon: engineer, bgGradient: "bg-bgEngineer", bgPosition: "bg-textColor"},
+  {name: "Operations", nums: 2, icon: operation, bgGradient: "bg-bgDesign", bgPosition: null},
+  {name: "People", nums: 6, icon: people, bgGradient: "bg-bgPeople", bgPosition: null},
+  {name: "Sales", nums: 7, icon: sales, bgGradient: "bg-bgSales", bgPosition: null},
+  {name: "University", nums: 8, icon: university, bgGradient: "bg-bgUniversity", bgPosition: null}
+]
+
+const App = () => {
+  return (
+    <main className='bg-bgColor min-h-screen flex flex-col justify-center items-center gap-6 border border-solid'>
+        <h1 className='text-textColor text-[2rem] font-black leading-normal font-Merriweather not-italic'>Open Positions</h1>
+        <section className='flex flex-row justify-center items-center w-[65rem] flex-wrap gap-6'>
+          {/* Job Card */}
+          {position.map((data, index) => {
+            return <JobCard key={index} {...data}/>
+          })}
+        </section>
+        <p className='heading-normal font-medium not-italic text-sm font-Poppins text-center text-textColor w-[52rem] opacity-70'>We are proud to be an equal opportunity employer that values diversity. We do not discriminate on the basis of race, religion, color, national origin, gender, sexual orientation, age, marital status, veteran status, or disability status.</p>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/JobCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Frame1 from '../dist/assets/Frame.png';
+
+const JobCard = ({name, nums, icon, bgGradient, bgPosition}) => {
+  return (
+    <div className="bg-white w-[18rem] py-10 px-7 flex flex-col justify-center items-center gap-8 rounded-2xl shadow-cardShadow hover:cursor-pointer">
+        <div className='flex items-center gap-4'>
+            <div className={`flex p-2 items-start gap-[0.625rem] rounded-[0.875rem] ${bgGradient}`}>
+                <img src={ icon} alt="Design" />
+            </div>
+            <h2 className='text-textColor leading-normal text-xl font-Poppins'>{ name }</h2>
+        </div>
+        <div className={`flex py-2 px-3 justify-center items-center gap-[0.625rem] self-stretch rounded-lg border border-solid border-[#C2C5E2] ${bgPosition ? bgPosition + " text-[#fff]" : "bg-white text-textColor"}`}>
+            <h2 className='font-Poppins leading-normal font-medium text-[1rem]'><span>{nums}</span> open positions</h2>
+        </div>
+        
+    </div>
+  )
+}
+
+export default JobCard
