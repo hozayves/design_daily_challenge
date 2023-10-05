d106e0a71dca981e02647f38456946916f3c95d6
hozayves <muhozayves2014@gmail.com>
2023-10-05 20:26:20 +0200

    add: mobile responsive :rage::innocent:

--- product-info/src/App.jsx
+++ product-info/src/App.jsx
@@ -5,33 +5,26 @@ import productImage from './assets/product.png'
 function App() {
 
   return (
-    <section className='max-w-[55.4375rem] min-w-fit rounded-[0.9375rem] bg-white shadow-cardShadow'>
-      <div>
-        <img src={ productImage } alt="" />
+    <section className='max-w-[55.4375rem] min-w-fit min-h-fit rounded-[0.9375rem] bg-white shadow-cardShadow flex-shrink flex flex-col gap-6'>
+      <div className='min-h-1/4 p-6'>
+        <img className='w-full h-1/4' src={ productImage } alt="" />
       </div>
-      <div>
-        <span>Free shipping</span>
-        <h1>Razer Kraken Kitty Edt Gaming Headset Quartz</h1>
-        <div>
-          <span>1.599,-</span>
-          <span>799,-</span>
-          <p>The offer is valid until April 3 or as long as stock lasts!</p>
+      <div className='px-6 flex flex-col items-start justify-center gap-3 pb-6'>
+        <div className=' w-full flex justify-between items-center'>
+          <span className='px-[0.5rem] py-[0.25rem] inline-flex items-start gap-[0.625rem] rounded-[1.25rem] bg-blue text-white leading-normal font-semibold text-sm font-Poppins not-italic'>Free shipping</span>
+          <ion-icon size="large" name="heart-outline"></ion-icon>
         </div>
-        <button>Add to cart</button>
-        <div>
-          <div>.</div>
-          <label>50+pcs.in stock.</label>
+        <h1 className='text-blue font-Poppins text-2xl not-italic font-semibold leading-normal'>Razer Kraken Kitty Edt Gaming Headset Quartz</h1>
+        <div className='flex flex-col w-full h-32 relative'>
+          <span className='text-blue font-Roboto text-lg not-italic leading-normal line-through absolute top-0'>1.599,-</span>
+          <span className='text-blue font-Poppins text-5xl not-italic font-bold leading-normal absolute top-3'>799,-</span>
+          <p className='text-blue font-Poppins text-sm not-italic font-normal leading-normal opacity-60 bottom-0 absolute'>The offer is valid until April 3 or as long as stock lasts!</p>
         </div>
-        <div>
-          <button>
-          <ion-icon name="cart-outline"></ion-icon>
-            <span>Add to cart</span>
-          </button>
-          <button>
-          <ion-icon name="heart-outline"></ion-icon>
-            <span>Add to wishlist</span>
-          </button>
+        <div className='flex gap-2 mt-5 justify-center items-center'>
+          <div className='w-4 h-4 bg-[#00D98B] rounded-full'>.</div>
+          <span className='text-[#000] font-Poppins text-sm not-italic font-medium leading-normal'>50+pcs.in stock.</span>
         </div>
+        <button className='flex p-4 justify-center items-center gap-[0.625rem] self-stretch rounded-lg bg-[#4788E9] shadow-buttonShadow text-white font-Poppins font-medium text-lg'>Add to cart</button>
       </div>
     </section>
   )
--- product-info/tailwind.config.js
+++ product-info/tailwind.config.js
@@ -7,8 +7,17 @@ export default {
   theme: {
     boxShadow: {
       "cardShadow": "0px 24px 28px 9px rgba(179, 193, 217, 0.33)",
+      "buttonShadow": "0px 7px 0px 0px #1C57EE"
     },
-    extend: {},
+    fontFamily: {
+      Poppins: ["Poppins", 'sans-serif'],
+      Roboto: ["Roboto", 'sans-serif']
+    },
+    extend: {
+      colors: {
+        blue: "var(--191847, #191847)"
+      }
+    },
   },
   plugins: [],
 }
